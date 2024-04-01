2fa00b0c2e0f0c43f1708291a1c931849e157dd1
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2024-04-01 12:16:34 +0530

    Creating Sidebar

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -2,12 +2,14 @@ import React, { ReactNode } from "react";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 
-const HomeLayout = ({ children }: { children: ReactNode }) => {
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="relative">
-      <Navbar/>
+      <Navbar />
+      {/* <p className="text-white">Navbar</p> */}
       <div className="flex">
-        <Sidebar/>
+        {/* <p className="text-white">Sidebar</p> */}
+        <Sidebar />
         <section className="flex min-h-screen flex-1 flex-col p-6 pb-6 pt-28 max-md:pb-16 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
